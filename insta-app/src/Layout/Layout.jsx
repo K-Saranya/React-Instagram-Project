a86e82698aa65eb0cb804f6a0b2fd95636cabde6
Tamilselvan1811 <tamil18112003@gmail.com>
2025-06-11 18:42:34 +0530

    routing completed

--- insta-app/src/Layout/Layout.jsx
+++ insta-app/src/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Content from "../components/Content/Content";
 import Navbar from "../components/Navbar/Navbar";
+import "./Layout.css";
 
 const Layout = () => {
   return (
